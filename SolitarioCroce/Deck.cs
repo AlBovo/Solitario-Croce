617da8ab4df85b14ac0789dc1be0b85c884ff88e
Marco Balducci <balduccimarco2007@gmail.com>
2024-05-07 10:39:26 +0200

    added Card and Deck class

---
+++ SolitarioCroce/Deck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitarioCroce
+{
+    class Card
+    {
+        /// <summary>
+        /// class Card, represents a placeable card on the table.
+        /// Seed guide:
+        /// - D = Denari
+        /// - C = Coppe
+        /// - S = Spade
+        /// - B = Bastoni
+        /// </summary>
+        public char seed { get; }
+        public int value { get; }
+
+        public Card(char seed, int value)
+        {
+            this.seed = seed;
+            this.value = value;
+        }
+    }
+    class Deck
+    {
+        private Queue<Card> deck = new Queue<Card>();
+        
+        public Deck()
+        {
+            for(int i = 0; i < 40; i++)
+            {
+                
+            }
+        }
+    }
+}
