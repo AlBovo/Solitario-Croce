248f52355a4b06bfb1fbc75d0e26931f11890c0e
Alan Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-05-22 09:47:31 +0200

    added iframe that must be finished

--- SolitarioCroce/rules.xaml.cs
+++ SolitarioCroce/rules.xaml.cs
@@ -23,6 +23,17 @@ namespace SolitarioCroce
         public Rules()
         {
             InitializeComponent();
+            LoadWebView();
+        }
+
+        private async void LoadWebView()
+        {
+            await iframe.EnsureCoreWebView2Async();
+            iframe.NavigateToString(
+                @"<html>" +
+                @"<iframe width=""400"" height=""400"" src=""https://www.youtube.com/embed/g7TJviLmuMg"" title=""Solitario Croce - Video dimostrativo - Solitari Free"" frameborder=""0"" allow=""accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"" referrerpolicy=""strict-origin-when-cross-origin"" allowfullscreen></iframe>" +
+                @"</html>"
+            );
         }
     }
 }
