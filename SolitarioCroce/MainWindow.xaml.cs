ad2d5bf92aa1149dba322a4e69325d295f64a9cd
Marco Balducci <balduccimarco2007@gmail.com>
2024-05-28 11:16:04 +0200

    added music

--- SolitarioCroce/MainWindow.xaml.cs
+++ SolitarioCroce/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
 
 namespace SolitarioCroce
 {
@@ -6,20 +9,51 @@ namespace SolitarioCroce
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    
+
     public partial class MainWindow : Window
     {
+
         public static int times = 0;
         public MainWindow()
         {
             InitializeComponent();
-            if(times == 5) easterEgg();
+            if (times == 5) easterEgg();
+            else
+            {
+                string relativePath = @".\music\soundtrack.mp3";
+                try
+                {
+                    BackgroundMusic.Source = new Uri(relativePath, UriKind.Relative);
+                    BackgroundMusic.MediaEnded += BackgroundMusic_MediaEnded; // Riproduci in loop
+                    BackgroundMusic.Play();
+                }
+                catch { MessageBox.Show("errore"); }
+            }
+
         }
 
         public void easterEgg()
         {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "music/easterEgg.mp3");
+            if (File.Exists(path))
+            {
+                try
+                {
+                    BackgroundMusic.Source = new Uri(path, UriKind.Absolute);
+                    BackgroundMusic.MediaEnded += BackgroundMusic_MediaEnded; // Riproduci in loop
+                    BackgroundMusic.Play();
+                }
+                catch { MessageBox.Show("errore"); }
+            }
+
             byte[] data = Convert.FromBase64String("RScgdG9ybmF0byBaZWI4OSEhIQ==");
             txt_easterEgg.Text = System.Text.Encoding.UTF8.GetString(data);
+        }
+
+        private void BackgroundMusic_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            BackgroundMusic.Position = TimeSpan.Zero;
+            BackgroundMusic.Play();
         }
 
         public void Game_Click(object sender, RoutedEventArgs e)
