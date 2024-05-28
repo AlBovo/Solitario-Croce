7fc59ae0c0966250a4bf670c4e17ceabd21cfa2f
Marco Balducci <balduccimarco2007@gmail.com>
2024-05-28 10:32:48 +0200

    added music and easterEgg

--- SolitarioCroce/MainWindow.xaml.cs
+++ SolitarioCroce/MainWindow.xaml.cs
@@ -5,11 +5,21 @@ namespace SolitarioCroce
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+        public static int times = 0;
         public MainWindow()
         {
             InitializeComponent();
+            if(times == 5) easterEgg();
+        }
+
+        public void easterEgg()
+        {
+            byte[] data = Convert.FromBase64String("RScgdG9ybmF0byBaZWI4OSEhIQ==");
+            txt_easterEgg.Text = System.Text.Encoding.UTF8.GetString(data);
         }
 
         public void Game_Click(object sender, RoutedEventArgs e)
@@ -21,6 +31,7 @@ namespace SolitarioCroce
 
         public void Rules_Click(object sender, RoutedEventArgs e)
         {
+            times++;
             Rules rules = new Rules();
             this.Close();
             rules.Show();
